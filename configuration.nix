eea293ad8da80485bdbec5a563a6bb1669999ad5
tomkyriacou64 <tomkyriacou64@gmail.com>
2023-06-30 11:31:27 +1000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -100,27 +100,25 @@
 
 
   # enable flatpak support
-  services.flatpak.enable = true;
-  services.dbus.enable = true;
-  security.polkit.enable = true;
- systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-  };
-   extraConfig = ''
-     DefaultTimeoutStopSec=10s
-   '';
-}; 
+#   security.polkit.enable = true;
+#  systemd = {
+#   user.services.polkit-gnome-authentication-agent-1 = {
+#     description = "polkit-gnome-authentication-agent-1";
+#     wantedBy = [ "graphical-session.target" ];
+#     wants = [ "graphical-session.target" ];
+#     after = [ "graphical-session.target" ];
+#     serviceConfig = {
+#         Type = "simple";
+#         ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+#         Restart = "on-failure";
+#         RestartSec = 1;
+#         TimeoutStopSec = 10;
+#       };
+#   };
+#    extraConfig = ''
+#      DefaultTimeoutStopSec=10s
+#    '';
+# }; 
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
