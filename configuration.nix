c629ad289c26c08434cf7b8bfe235566d6b5aa11
tomkyriacou64 <tomkyriacou64@gmail.com>
2023-07-05 15:54:12 +1000

    add tailscale

--- configuration.nix
+++ configuration.nix
@@ -119,10 +119,57 @@
 
   # Enable Services
   services.gvfs.enable = true;
+  
+  
 
-  # Disable the firewall altogether.
-  networking.firewall.enable = false;
-  networking.enableIPv6 = false;
+  # Setup and Configure Tailscale
+
+  # Enable the Tailscale service
+  services.tailscale.enable = true;
+
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey tskey-auth-kYHftE6CNTRL-Jd8VUSFtyegMTL2AE1V4fg71FNJNeMcTK
+    '';
+  };
+
+
+  networking.firewall = {
+    # enable the firewall
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [ config.services.tailscale.port ];
+
+    # allow you to SSH in over the public internet
+    allowedTCPPorts = [ 22 ];
+  };
+
 
 
   # Copy the NixOS configuration file and link it from the resulting system
